141681db3a4f3e9682446dee9c709af22c6ca56f
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-04-30 13:48:09 +0000

    Initialize db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+Implementationof the database storage engine
+
+"""
+import unittest
+
+
+class DBStorage(unittest.TestCase):
+    """
+    Implementation of the database storage engine
+    """
+    pass
