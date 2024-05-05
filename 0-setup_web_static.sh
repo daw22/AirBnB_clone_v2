e816463f4462d04a3dbfa3e6823bae0cd4bff7f8
Dawit Yifru <dawitxxd@gmail.com>
2024-05-05 14:03:24 +0300

    prepare webserver for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,37 @@ apt update -y
 apt install -y nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "Holberton School" | tee /data/web_static/releases/test/index.html
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://youtube.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
