5a5205008e57d4bdc62a90fd3f76d114bdfdd9ce
Dawit Yifru <dawitxxd@gmail.com>
2024-05-06 13:08:16 +0300

    ....

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,26 +9,10 @@ echo "<!DOCTYPE html><html><head></head><body><p>Nginx server test</p></body></h
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://youtube.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+path="/etc/nginx/sites-available/default"
+dir="location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}"
+if ! grep -q "location /hbnb_static/" $conf_path
+then
+    sudo sed -i "s/^\tserver_name _;/\tserver_name _;\n\t$dir/" $path
+fi
 sudo service nginx restart
