6b0364c2d1838b36c5b674d9325422aea0b58743
Dawit Yifru <dawitxxd@gmail.com>
2024-05-06 10:58:02 +0300

    ....

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,14 +5,7 @@ apt update -y
 apt install -y nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
+echo "<!DOCTYPE html><html><head></head><body><p>Nginx server test</p></body></html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 
