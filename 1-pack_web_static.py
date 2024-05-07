a151899731a77dc03660f91d488e56c2be157f4d
Dawit Yifru <dawitxxd@gmail.com>
2024-05-07 17:17:29 +0300

    pack web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -19,7 +19,7 @@ def do_pack():
     dest = local("mkdir -p versions")
     file_name = str(datetime.now()).replace(" ", "")
     options = re.sub(r'[^\w\s]', '', file_name)
-    tar = local('tar -cvzf versions/web_static_{}.tgz'.format(options))
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(options))
     if os.path.exists("./versions/web_static_{}.tgz".format(options)):
         return os.path.normpath("/versions/web_static_{}.tgz".format(options))
     else:
